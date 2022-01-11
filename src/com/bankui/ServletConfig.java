8e02c358cb7d80e7746d7c1ec24187b9828651e6
Rutika Waghose <waghoserutika17@gmail.com>
2022-01-11 11:09:58 +0530

    spring form added

---
+++ src/com/bankui/ServletConfig.java
@@ -0,0 +1,22 @@
+package com.bankui;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.bankui.controller"})
+public class ServletConfig implements WebMvcConfigurer{
+
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("WEB-INF/jsps/"); //WEB-INF/jsps/index.jsp
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
